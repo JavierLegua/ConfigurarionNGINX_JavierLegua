4ca433143558748f284c8c5964d2b86b0c366b2e
Javier Legua <javierlegua14@gmail.com>
2022-02-03 09:14:16 +0100

    Ficheros

---
+++ ConfiguracionNGINX_JavierLegua/mvc_completo/App/config/configurar.php
@@ -0,0 +1,16 @@
+<?php
+
+    // Ruta de la aplicacion
+    define('RUTA_APP', dirname(dirname(__FILE__)));
+
+    // Ruta url, Ejemplo: http://localhost/daw2_mvc
+    define('RUTA_URL', 'https://192.168.4.165');
+
+    define('NOMBRE_SITIO', 'CRUD MVC - DAW2 Alcañiz');
+
+
+    // Configuracion de la Base de Datos
+    define('DB_HOST', 'localhost');
+    define('DB_USUARIO', 'pma');
+    define('DB_PASSWORD', 'kamisama123');
+    define('DB_NOMBRE', 'crud_mvc');
